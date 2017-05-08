870dc1106e5c080aa3fd287ec4e36c57866ea9ba
Carl Masak <cmasak@gmail.com>
2017-05-08 18:31:26 +0200

    Handle opening/closing tags

--- src/anglerfish.js
+++ src/anglerfish.js
@@ -11,5 +11,49 @@ export function lineAndColumn(content, index) {
 }
 
 export function parseTemplate(content, fileName) {
-    return [];
+    let errors = [];
+
+    let pos = 0;
+    let tagStack = [];
+
+    function registerError(message) {
+        let [line, column] = lineAndColumn(content, pos);
+        errors.push({ message, fileName, line, column });
+    }
+
+    while (pos < content.length) {
+        let spaceCount = content.substring(pos).match(/^\s*/)[0].length;
+        pos += spaceCount;
+
+        if (pos >= content.length) {
+            break;
+        }
+
+        let suffix = content.substring(pos);
+        let openingTagMatch = suffix.match(/<(\w+)>/);
+        let closingTagMatch = suffix.match(/<\/(\w+)>/);
+
+        if (openingTagMatch) {
+            let [{ length }, tagName] = openingTagMatch;
+            tagStack.push(tagName);
+            pos += length;
+        } else if (closingTagMatch) {
+            let [{ length }, tagName] = closingTagMatch;
+            let expectedTagName = tagStack.pop();
+            while (tagName !== expectedTagName) {
+                registerError(`Got </${tagName}> before the expected </${expectedTagName}>`);
+                expectedTagName = tagStack.pop();
+            }
+            pos += length;
+        } else {
+            throw new Error(`Don't know how to handle "${suffix}"`);
+        }
+    }
+
+    if (tagStack.length) {
+        let expectedTagName = tagStack.pop();
+        registerError(`Got end of template before the expected </${expectedTagName}>`);
+    }
+
+    return errors;
 }
--- test/parse-html.js
+++ test/parse-html.js
@@ -1,8 +1,48 @@
 import assert from "assert";
 import { parseTemplate } from "../src/anglerfish";
 
+function deindent(string) {
+    string = string.replace(/^\n+/, "").replace(/\s+$/, "");
+    let firstLineIndent = string.match(/[ ]*/)[0].length;
+    return string.split(/\n/).map((line) => {
+        let indent = line.match(/[ ]*/)[0].length;
+        if (indent < firstLineIndent) {
+            throw new Error("Subsequent line is indented less than first line");
+        }
+        return line.substring(firstLineIndent);
+    }).join("\n");
+}
+
 describe("Parsing a HTML template", () => {
     it("accepts an empty template as input", () => {
         assert.deepEqual(parseTemplate("", "x.html"), []);
+    });
+
+    it("accepts matching opening and closing tags", () => {
+        assert.deepEqual(parseTemplate("<div></div>", "x.html"), []);
+        assert.deepEqual(parseTemplate("<div><p></p></div>", "x.html"), []);
+    });
+
+    it("rejects a closing tag when it expected another", () => {
+        let content = deindent(`
+            <div>
+              <p>
+            </div>
+        `);
+
+        assert.deepEqual(parseTemplate(content, "x.html"), [
+            { message: "Got </div> before the expected </p>", fileName: "x.html", line: 3, column: 1 },
+        ]);
+    });
+
+    it("rejects a document that doesn't close all its opened elements", () => {
+        let content = deindent(`
+            <div>
+              <p></p>
+        `);
+
+        assert.deepEqual(parseTemplate(content, "x.html"), [
+            { message: "Got end of template before the expected </div>", fileName: "x.html", line: 2, column: 10 },
+        ]);
     });
 });
