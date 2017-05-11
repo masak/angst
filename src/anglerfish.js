49503495602c065a9147ef3665e905b1d35bc4e2
Carl Masak <cmasak@gmail.com>
2017-05-11 14:32:00 +0200

    Simplify code a bit

--- src/anglerfish.js
+++ src/anglerfish.js
@@ -1,11 +1,7 @@
 export function lineAndColumn(content, index) {
     let prefix = content.substring(0, index);
     let line = prefix.split(/\n/).length;
-
-    let indexOfLastNewline = prefix.lastIndexOf("\n");
-    let column = indexOfLastNewline === -1
-        ? prefix.length + 1
-        : prefix.length - indexOfLastNewline;
+    let column = prefix.length - prefix.lastIndexOf("\n");
 
     return [line, column];
 }
